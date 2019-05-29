5840271aa16f917e8c9ece275cdf7975fdd57fca
Victor <Victor_UA@Ukr.net>
2019-05-29 21:31:16 +0300

    Done

--- CBS.011/Program.cs
+++ CBS.011/Program.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CBS._011
 {
+    //Написать класс "ThreadWithResult<TReturn>", который будет иметь методы/свойства:
+
+    //- IsCompleted // Операция завершена или нет
+    //- Success // Операция завершена успешно или нет
+    //- Result // Результат операции
+    //- Start() // Запуск операции
+
+    //- Операцию для выполнения передавать в конструктор с помощью делегата
+    //- Класс должен быть generic.Generic-параметр указывает тип результата операции.
+    //- Result - должен выдавать результат операции или исключение если оно произошло внутри переданной операции
+    //или если операция еще не запускалась
+
+
+
+    //- Добавить возможность создания экземпляров с помощью записи: 
+    //"var thread = ThreadWithResult.Create(()=>10)" (пример),
+    //с автоматическим определением типа возвращаемого значения, с помощью переданного делегата.
     class Program
     {
         static void Main(string[] args)
         {
+            var thread = ThreadWithResult.Create(() => 10);
+
+            Console.WriteLine(thread);
+
+            new Thread(thread.Start).Start();
+
+            Console.WriteLine(thread);
+
+            while (!thread.IsCompleted)
+            {
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine(thread);
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ CBS.011/ThreadWithResult.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CBS._011
+{
+    public class ThreadWithResult<T>
+    {
+
+        internal ThreadWithResult(Func<T> threadStart)
+        {
+            _threadStart = threadStart;
+            IsCompleted = false;
+            Success = false;
+        }
+
+        private const string ERROR_MESSAGE_NO_RESULT = "The thread have not received a result yet";
+
+        private readonly Func<T> _threadStart;
+        private T _result;
+
+        public bool IsCompleted { get; private set; }
+        public bool Success { get; private set; }
+
+        public T Result
+        {
+            get => Success && IsCompleted ? _result : throw new Exception(ERROR_MESSAGE_NO_RESULT);
+            private set => _result = value;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                Result = _threadStart();
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");         
+            }
+            IsCompleted = true;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            try
+            {
+                result = Result.ToString();
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+            }            
+            return $"IsCompleted: {IsCompleted}, Success: {Success}, Result: {result}";
+        }
+    }
+
+
+    public static class ThreadWithResult
+    {
+        public static ThreadWithResult<T> Create<T>(Func<T> threadStart)
+        {
+            return new ThreadWithResult<T>(threadStart);
+        }
+    }
+}
+
